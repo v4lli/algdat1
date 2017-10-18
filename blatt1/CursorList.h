cd479117826f35b398ce021bc87e773f5bc0ed39
Valentin Dornauer <valentin@unimplemented.org>
2017-10-18 14:58:41 +0200

    Iteratorfunktionen in O(1)

--- blatt1/CursorList.h
+++ blatt1/CursorList.h
@@ -83,6 +83,21 @@ public:
 	T& front() const {
 		int foo = data[start_data].data;
 		return foo;
+		// XXX
+	}
+
+private:
+	int find_free() {
+		// find next free element XXX make more efficient
+		int free = -1;
+		for (int i = 0; i < SIZE; i++) {
+			struct item *current = data[i];
+			if (current->next == -1 && current->prev == -1) {
+				free = i;
+				break;
+			}
+		}
+		return free;
 	}
 
 	void push_front(const T &param) // add a new value to the front of a list
@@ -98,21 +113,11 @@ public:
 		data[start_free].prev = -1;
 
 		data[start_data].prev = start_free;
-		start_data = start_free;
-
-		// find next free element XXX make more efficient
-		start_free = -1;
-		for (int i = 0; i < SIZE; i++) {
-			struct item *current = data[i];
-			if (current->next == -1 && current->prev == -1) {
-				start_free = i;
-				break;
-			}
-		}
+		start_data = find_free();
 	}
 
-	void pop_front()
-	{
+public:
+	void pop_front() {
 		int deleted = start_data;
 
 		start_data = data[deleted].next;
@@ -124,9 +129,79 @@ public:
 		start_free = 0;
 	}
 
-	iterator begin() const;
-	iterator end() const;
-	iterator insert(iterator itr, const T& value); // insert before itr
-	iterator erase(iterator start, iterator stop); // stop exclusive
-	iterator erase(iterator itr); // return ++itr
+	iterator begin() const {
+		return new iterator(0);
+	}
+
+	iterator end() const {
+		// XXX passt?
+		return new iterator(SIZE - 1);
+	}
+
+	// sollen constant-time benoetigen:
+	iterator insert(iterator itr, const T& value) // insert before itr
+	{
+		if (start_free < 0) {
+			// XXX exception
+			return NULL;
+		}
+
+		int idx = *itr; // via operator-overloading
+		assert(idx <= SIZE);
+
+		struct item *current = &data[idx];
+		struct item *prev = &data[current->prev];
+		struct item *free = &data[start_free];
+
+		free->prev = current->prev;
+		free->next = idx;
+		free->data = *value;
+
+		prev->next = start_free;
+		current->prev = start_free;
+
+		start_free = find_free();
+
+		return itr;
+	}
+
+	iterator erase(iterator start, iterator stop) // stop exclusive
+	{
+		int start_idx = *start;
+		int stop_idx = *stop;
+
+		// XXX handle specialcases:
+		// - start = start of list
+		// - end = end of list
+		//
+		// XXX asserts
+
+		struct item *start_item = &data[start_idx];
+		int next = start_idx;
+		struct item *end_item = &data[stop_idx];
+		struct item *prev_to_start = &data[start_item->prev];
+
+		prev_to_start->next = end_item->next;
+		start_item->prev = -1;
+		end_item->prev = start_item->prev;
+		end_item->next = -1;
+
+		struct item *current = NULL;
+		while (true) {
+			current = &data[next];
+
+			next = current->next;
+			current->next = -1;
+			current->prev = -1;
+			current->data = 0;
+
+			if (next == -1)
+				break;
+		}
+		return stop;
+	}
+	iterator erase(iterator itr) // return ++itr
+	{
+		return erase(itr, ++itr);
+	}
 };
