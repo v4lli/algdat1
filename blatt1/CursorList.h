996f09a3bc0e67389eb66b740a9ad9e2be0f5cad
Valentin Dornauer <valentin@unimplemented.org>
2017-10-24 15:40:57 +0200

    Aufgabe 3/4 fertig inkl Testprogramm

--- blatt1/CursorList.h
+++ blatt1/CursorList.h
@@ -2,6 +2,9 @@
 #include <iterator>     // std::iterator, std::input_iterator_tag
 #include <cassert>
 
+#define ITERATOR_END -2
+#define SLOT_EMPTY -1
+
 template <class T> class CursorIterator
 {
 private:
@@ -13,22 +16,22 @@ private:
 	int idx;
 	struct item *data;
 
-public:
-	CursorIterator(void *storage, int start_at = 0) {
-		data = (struct item *)storage;
-		// XXX unschoen, keine voidpointer am besten
-		idx = start_at;
-		//printf("new iterator with dataptr=%p\n", data);
+	void increment() {
+		if (idx == ITERATOR_END)
+			throw std::logic_error("Increment iterator end\n");
+
+		idx = (data + getIdx())->next;
+		if (idx < 0)
+			idx = ITERATOR_END;
 	}
 
+public:
 	typedef CursorIterator<T> iterator;
 
-	T& operator* () {
-		return (data + idx)->data;
-	}
-
-	iterator& operator= (const iterator& rhs) {
-		return rhs;
+	CursorIterator(void *storage, int start_at = 0) {
+		// XXX unschoen, keine opaque pointer sondern struct item
+		data = (struct item *)storage;
+		idx = start_at;
 	}
 
 	int getIdx() const {
@@ -39,18 +42,33 @@ public:
 		return data;
 	}
 
+	T& operator* () {
+		if (idx == ITERATOR_END)
+			throw std::logic_error("Dereferenced iterator end\n");
+		else
+			return (data + getIdx())->data;
+	}
+
+	iterator& operator= (const iterator& rhs) {
+		auto itr = new CursorIterator<T>(rhs.getDataPtr(), rhs.getIdx());
+		return itr;
+	}
+
 	bool operator!= (const iterator& rhs) const {
 		return (getIdx() != rhs.getIdx()) || (getDataPtr() != rhs.getDataPtr());
 	}
+
 	bool operator== (const iterator& rhs) const {
 		return (getIdx() == rhs.getIdx()) && (getDataPtr() == rhs.getDataPtr());
 	}
-	iterator& operator++ () {
-		idx++;
-		return this;
+
+	iterator& operator++ () { // prefix increment operator
+		increment();
+		return *this;
 	}
-	iterator operator++ (int) {// postfix operator, dummy parameter
-		idx++;
+
+	iterator operator++ (int) { // postfix increment operator
+		increment();
 		return this;
 	}
 };
@@ -71,22 +89,24 @@ private:
 	typedef CursorIterator<T> iterator;
 
 public:
-	List() : start_data(-1), start_free(0) {
+	List() : start_data(SLOT_EMPTY), start_free(0) {
 		// initialize list with correct, empty valyes
 		struct item empty;
-		empty.next = -1;
-		empty.prev = -1;
+		empty.next = SLOT_EMPTY;
+		empty.prev = SLOT_EMPTY;
 		empty.data = 0;
 		for(int i = 0; i < SIZE; i++)
 			memcpy(&data[i], &empty, sizeof(struct item));
 
+#ifdef DEBUG
 		printf("Initialized new List with %d elements; sizeof(data)=%ld\n",
 			SIZE, sizeof(data));
+#endif
 	}
 
 	typedef T value_type;
 	bool empty() const {
-		if (start_data == -1)
+		if (start_data == SLOT_EMPTY)
 			return true;
 		else
 			return false;
@@ -94,13 +114,12 @@ public:
 
 	int size() const
 	{
-		if (start_data == -1)
+		if (start_data == SLOT_EMPTY)
 			return 0;
 
 		const struct item *current = &data[start_data];
 		int counter = 1;
-		while (current->next != -1) {
-			//printf("current->next=%d\n", current->next);
+		while (current->next != SLOT_EMPTY) {
 			counter++;
 			assert(current->next <= SIZE);
 			current = &data[current->next];
@@ -112,16 +131,17 @@ public:
 	T& front() const {
 		int foo = data[start_data].data;
 		return foo;
-		// XXX
+		// XXX richtig schlecht
 	}
 
 private:
 	int find_free() {
-		// find next free element XXX make more efficient
-		int free = -1;
+		int free = SLOT_EMPTY;
 		for (int i = 0; i < SIZE; i++) {
 			struct item *current = &data[i];
-			if (current->next == -1 && current->prev == -1 && i != start_data) {
+			if (current->next == SLOT_EMPTY
+			    && current->prev == SLOT_EMPTY
+			    && i != start_data) {
 				free = i;
 				break;
 			}
@@ -129,62 +149,78 @@ private:
 		return free;
 	}
 
+	int getLastElem() const
+	{
+		if (start_data == SLOT_EMPTY)
+			return 0;
+
+		const struct item *current = &data[start_data];
+		int last = 0;
+		while (current->next != SLOT_EMPTY) {
+			last = current->next;
+			assert(current->next <= SIZE);
+			current = &data[current->next];
+		}
+
+		return last;
+	}
+
 public:
 
 	void push_front(const T &param) // add a new value to the front of a list
 	{
 
-		if (start_free == -1) {
-			// XXX exception?
-			return;
-		}
+		if (start_free == SLOT_EMPTY)
+			throw std::runtime_error("List full");
 
 		if (empty()) {
-			data[start_free].next = -1;
-			data[start_free].prev = -1;
+			data[start_free].next = SLOT_EMPTY;
+			data[start_free].prev = SLOT_EMPTY;
 			data[start_free].data = param;
 		} else {
 			data[start_free].next = start_data;
-			data[start_free].prev = -1;
+			data[start_free].prev = SLOT_EMPTY;
 			data[start_free].data = param;
 			data[start_data].prev = start_free;
 		}
 
 		start_data = start_free;
 		start_free = find_free();
-		//printf("Added new element to the front; start_free=%d, start_data=%d\n", start_free, start_data);
 	}
 
-	// macht noch was falsches... XXX ist aktuell pop_back()
 	void pop_front() {
 		int deleted = start_data;
 
 		start_data = data[deleted].next;
 
 		data[deleted].data = 0;
-		data[deleted].prev = -1;
-		data[deleted].next = -1;
+		data[deleted].prev = SLOT_EMPTY;
+		data[deleted].next = SLOT_EMPTY;
 
 		start_free = deleted;
 	}
 
 	iterator begin() const {
-		return iterator((void*)&data, 0);
+		int iterator_start;
+		if (start_data == SLOT_EMPTY)
+			iterator_start = ITERATOR_END;
+		else
+			iterator_start = start_data;
+
+		return iterator((void*)&data, iterator_start);
 	}
 
 	iterator end() const {
-		return iterator((void*)&data, size());
+		return iterator((void*)&data, ITERATOR_END);
 	}
 
 	// sollen constant-time benoetigen:
 	iterator insert(iterator itr, const T& value) // insert before itr
 	{
-		if (start_free < 0) {
-			// XXX exception
-			return NULL;
-		}
+		if (start_free < 0)
+			throw std::runtime_error("List full");
 
-		int idx = *itr; // via operator-overloading
+		int idx = itr.getIdx();
 		assert(idx <= SIZE);
 
 		struct item *current = &data[idx];
@@ -193,7 +229,7 @@ public:
 
 		free->prev = current->prev;
 		free->next = idx;
-		free->data = *value;
+		free->data = value;
 
 		prev->next = start_free;
 		current->prev = start_free;
@@ -205,8 +241,9 @@ public:
 
 	iterator erase(iterator start, iterator stop) // stop exclusive
 	{
-		int start_idx = *start;
-		int stop_idx = *stop;
+		int start_idx = start.getIdx();
+		assert(start_idx >= 0);
+		int stop_idx = stop.getIdx();
 
 		// XXX handle specialcases:
 		// - start = start of list
@@ -215,31 +252,40 @@ public:
 		// XXX asserts
 
 		struct item *start_item = &data[start_idx];
-		int next = start_idx;
-		struct item *end_item = &data[stop_idx];
 		struct item *prev_to_start = &data[start_item->prev];
+		int next = start_idx;
+		start_item->prev = SLOT_EMPTY;
 
-		prev_to_start->next = end_item->next;
-		start_item->prev = -1;
-		end_item->prev = start_item->prev;
-		end_item->next = -1;
+		if (stop_idx == ITERATOR_END) {
+			prev_to_start->next = SLOT_EMPTY;
+		} else {
+			struct item *end_item = &data[stop_idx];
+			prev_to_start->next = end_item->next;
 
+			end_item->prev = start_item->prev;
+			end_item->next = SLOT_EMPTY;
+		}
+
+		// mark possibly new emptuy slots as such
 		struct item *current = NULL;
 		while (true) {
 			current = &data[next];
 
 			next = current->next;
-			current->next = -1;
-			current->prev = -1;
+			current->next = SLOT_EMPTY;
+			current->prev = SLOT_EMPTY;
 			current->data = 0;
 
-			if (next == -1)
+			if (next == SLOT_EMPTY)
 				break;
 		}
 		return stop;
 	}
+
 	iterator erase(iterator itr) // return ++itr
 	{
 		return erase(itr, ++itr);
 	}
 };
+
+// XXX iterator invalidation bei insert bzw erase
--- blatt1/CursorMain.cpp
+++ blatt1/CursorMain.cpp
@@ -9,18 +9,20 @@
 		printf("%d, ", *it); \
 	printf("\n############### DUMP END ###############\n"); \
 }
-//printf("lst.front() = %d\n", lst.front());\
 
 int main(void) {
 	auto lst = List<int, 128>();
 
-	//DUMP();
-	printf("push_front()\n");
+	// test push_front(), empty(), size() and pop_front(), iterator.begin()
+	// iterator.end(), iterator* and iterator++
+	DUMP();
+	printf("push_front() 1\n");
 	lst.push_front(1);
-	printf("push_front()\n");
+	printf("push_front() 2\n");
 	lst.push_front(2);
-	printf("push_front()\n");
+	printf("push_front() 3\n");
 	lst.push_front(3);
+	// list should now be: 3, 2, 1
 	DUMP();
 	printf("pop_front()\n");
 	lst.pop_front();
@@ -30,6 +32,43 @@ int main(void) {
 	printf("pop_front()\n");
 	lst.pop_front();
 	DUMP();
+	printf("push_front([10, 20, 30, 40, 50])\n");
+	lst.push_front(10);
+	lst.push_front(20);
+	lst.push_front(30);
+	lst.push_front(40);
+	lst.push_front(50);
+	DUMP();
+	CursorIterator<int> ins = lst.begin();
+	ins++;
+	printf("insert at pos1\n");
+	lst.insert(ins, 45);
+	DUMP();
+	printf("delete element 3-4 (40,30)\n");
+	CursorIterator<int> del = lst.begin();
+	del++;
+	del++;
+	CursorIterator<int> del_end = CursorIterator<int>(del);
+	del_end++;
+	lst.erase(del, del_end);
+	DUMP();
+	printf("delete all remaining elements\n");
+	lst.erase(lst.begin(), lst.end());
+	DUMP();
+	lst.push_front(1);
+	DUMP();
+	printf("delete one element\n");
+	lst.erase(lst.begin());
+	DUMP();
+	printf("\n\n");
+
+	// Test iterator copy constructor
+	CursorIterator<int> it=lst.begin();
+	CursorIterator<int> it2(it);
+	CursorIterator<int> it_end=lst.end();
+	CursorIterator<int> it2_end(it_end);
+	printf("it==it2: %d (should be 1)\n", it == it2);
+	printf("it2_end==it: %d (should be 0)\n", it2_end == it);
 
 	return 0;
 }
