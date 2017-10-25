36e7524f3068f28df704307c19569b9f34f216a1
Angelika Walter <angelika.walter@post-ist-da.de>
2017-10-25 12:13:45 +0200

    Test-Klasse für den Studenten

---
+++ blatt1/StudentMain.cpp
@@ -0,0 +1,49 @@
+/*
+ * StudentMain.cpp
+ *
+ *  Created on: 25.10.2017
+ *      Author: Angelika
+ */
+
+#include "StudnetMain.h";
+#include "Student.h";
+#include <cstdio>;
+#include <iostream>;
+using namespace std;
+
+int main(void)
+{
+	// Testen, ob die Setter und Getter funktionieren.
+	Student* s1 = new Student();
+	s1->setMatNummer(12345);
+	cout << "Should be '12345' and is:" << s1->getMatNummer() << endl;
+	s1->setName("Walter\0");
+	cout << "Should be 'Walter' and is:" << s1->getName() << endl;
+	s1->setVorname("Angelika\0");
+	cout << "Should be 'Angelika' and is:" << s1->getVorname() << endl;
+	s1->setGebTag("01.01.93\0");
+	cout << "Should be '01.01.93' and is:" << s1->getGebTag() << endl;
+
+	Student* s2 = new Student();
+	s2->setMatNummer(23456);
+	s2->setName("Wurst\0");
+	s2->setVorname("Hans\0");
+	s2->setGebTag("02.02.02\0");
+
+	// Testen, ob die 'ToString' funktioniert.
+	cout << s2 << endl;
+
+	// Vergleiche Testen.
+	cout << "Should be 'true' and is:" << (s2 > s1) << endl;
+	cout << "Should be 'true' and is:" << (s1 < s2) << endl;
+	cout << "Should be 'true' and is:" << (s2 >= s1) << endl;
+	cout << "Should be 'true' and is:" << (s1 <= s2) << endl;
+	cout << "Should be 'true' and is:" << (s2 != s1) << endl;
+	cout << "Should be 'true' and is:" << (s2 == s2) << endl;
+	cout << "Should be 'true' and is:" << (s1 >= s1) << endl;
+	cout << "Should be 'true' and is:" << (s1 <= s1) << endl;
+	cout << "Should be 'false' and is:" << (s2 == s1) << endl;
+	cout << "Should be 'false' and is:" << (s1 > s1) << endl;
+	cout << "Should be 'false' and is:" << (s1 < s1) << endl;
+	cout << "Should be 'false' and is:" << (s1 != s1) << endl;
+}
---
+++ blatt1/StudnetMain.h
@@ -0,0 +1,17 @@
+/*
+ * StudnetMain.h
+ *
+ *  Created on: 25.10.2017
+ *      Author: Angelika
+ */
+
+#ifndef STUDNETMAIN_H_
+#define STUDNETMAIN_H_
+
+class StudentMain{
+	int main(void);
+};
+
+
+
+#endif /* STUDNETMAIN_H_ */
